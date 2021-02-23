44a7174a55ed0947bd413567a7e4a028660b3d39
Romain Caron <caron.romain@free.fr>
2021-02-23 18:30:02 +0100

    Ajout et mise en place de l'espace de saisie
    Ajout de la zone de saisie et du bouton et début de la fonction onClick du bouton pour saisir les valeurs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,14 +20,42 @@ namespace TousUnixPourRaoul
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Stake> stakes = new List<Stake>();
+        int count_question = 1;
+        int nb_stakes = 0;
+        Stake temp_stake;
+        struct Stake
+        {
+            public float x;
+            public float y;
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public void main()
+        private void onClick(object sender, RoutedEventArgs e)
         {
-
+            //Veuillez saisir le nombre de piquets 
+            if(count_question == 1 && nb_stakes == 0) {
+                //initialisation de nb_stakes à x la valeur saisie par l'utilisateur
+            }
+            //Veuillez saisir la coordonnée x du piquet ...
+            if(count_question%2 == 0 && count_question < nb_stakes) {
+                //Le n-ième piquet est créé et son x est définit 
+            } else { //Veuillez saisir la cordonnée y du piquet
+                //Son y est définit
+                //Ajout du piquet dans la liste
+            }
+            //Juste avant de terminer la saisie
+            if(count_question == nb_stakes - 1) {
+                //Changement de la valeur OK du bouton en "Obtenir le résultat"
+            }
+            //Plus de piquet à saisir
+            if(count_question == nb_stakes) {
+                //Appel d'une fonction pour traiter les valeurs et afficher le résultat
+            }
+            count_question++;
         }
     }
 }
