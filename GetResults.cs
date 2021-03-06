1d297852054c73491c8175a64b32585ac69429e7
Romain Caron <caron.romain@free.fr>
2021-03-06 17:24:21 +0100

    Vérifications + saisie
    Ajout des vérifications + saisie des valeurs

---
+++ GetResults.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TousUnixPourRaoul
+{
+    public class GetResults
+    {
+
+        public string getSquare(List<MainWindow.Stake> stakes)
+        {
+            return "Aire : xxx";
+        }
+
+        public string getCenterOfGravity(List<MainWindow.Stake> stakes)
+        {
+            return "Centre de gravité : xxx";
+        }
+
+        public string getCowPosition(List<MainWindow.Stake> stakes)
+        {
+            return "Position de la vache : La vache est xxx";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TousUnixPourRaoul
 {
@@ -23,11 +12,12 @@ namespace TousUnixPourRaoul
         List<Stake> stakes = new List<Stake>();
         int count_question = 1;
         int nb_stakes = 0;
-        Stake temp_stake;
-        struct Stake
+        String error;
+        int temp_x;
+        public struct Stake
         {
-            public float x;
-            public float y;
+            public int x;
+            public int y;
         }
         public MainWindow()
         {
@@ -36,26 +26,87 @@ namespace TousUnixPourRaoul
 
         private void onClick(object sender, RoutedEventArgs e)
         {
-            //Veuillez saisir le nombre de piquets 
-            if(count_question == 1 && nb_stakes == 0) {
-                //initialisation de nb_stakes à x la valeur saisie par l'utilisateur
+            int value;
+            bool isValid = false;
+            if (int.TryParse(tbox_input.Text, out value))
+            {
+                //Veuillez saisir le nombre de piquets 
+                if (count_question == 1 && nb_stakes == 0)
+                {
+                    //Vérification de la valeur saisie (entre 1 et 50)
+                    if (value < 1 || value > 50)
+                    {
+                        error = "Veuillez entrer un nombre valide de piquet (1-50)";
+                        count_question = 1;
+                    } else {
+                        //Vérfication OK
+                        isValid = true;
+                        error = "";
+                        lb_input.Content = "Veuillez saisir la coordonnée x du piquet n°" + (stakes.Count + 1);
+                        nb_stakes = value;
+                    }
+                }
+
+                //Veuillez saisir la coordonnée x du piquet ...
+                if (count_question%2 == 0)
+                {
+                    temp_x = 0;
+                    lb_input.Content = "Veuillez saisir la coordonnée y du piquet n°" + (stakes.Count + 1);
+                    temp_x = value;
+                    isValid = true;
+                }
+                else
+                { //Veuillez saisir la cordonnée y du piquet
+                    if(count_question != 1)
+                    { 
+                        Stake temp_stake;
+                        temp_stake.x = 0;
+                        temp_stake.y = 0;
+
+                        temp_stake.x = temp_x;
+                        temp_stake.y = value;
+                        stakes.Add(temp_stake);
+                        lb_input.Content = "Veuillez saisir la coordonnée x du piquet n°" + (stakes.Count + 1);
+                        isValid = true;
+                    }
+                }
+
+                if(stakes.Count == nb_stakes - 1 && count_question%2 == 0 && isValid == true)
+                {
+                    btn_input.Content = "Terminer la saisie";
+                }
+
+                if(stakes.Count == nb_stakes && isValid == true)
+                {
+                    tbox_input.Text = "";
+                    lb_input.Content = "Saisie terminée";
+                    btn_input.IsEnabled = false;
+                    tbox_input.IsEnabled = false;
+
+                    //Résultats à afficher
+                    GetResults results = new GetResults();
+
+                    String square = results.getSquare(stakes);
+                    String center_of_gravity = results.getCenterOfGravity(stakes);
+                    String cow_position = results.getCowPosition(stakes);
+
+
+                    //Affichage des résultats
+                    gb_results.Visibility = Visibility.Visible;
+                    lb_square.Content = square;
+                    lb_cog.Content = center_of_gravity;
+                    lb_cow_position.Content = cow_position;
+                }
+                if(isValid)
+                {
+                    tbox_input.Text = "";
+                    count_question++;
+                }
+            } else
+            {
+                error = "Veuillez saisir un nombre";
             }
-            //Veuillez saisir la coordonnée x du piquet ...
-            if(count_question%2 == 0 && count_question < nb_stakes) {
-                //Le n-ième piquet est créé et son x est définit 
-            } else { //Veuillez saisir la cordonnée y du piquet
-                //Son y est définit
-                //Ajout du piquet dans la liste
-            }
-            //Juste avant de terminer la saisie
-            if(count_question == nb_stakes - 1) {
-                //Changement de la valeur OK du bouton en "Obtenir le résultat"
-            }
-            //Plus de piquet à saisir
-            if(count_question == nb_stakes) {
-                //Appel d'une fonction pour traiter les valeurs et afficher le résultat
-            }
-            count_question++;
+            lb_error.Content = error;
         }
     }
 }
